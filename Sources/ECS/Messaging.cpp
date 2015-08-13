025eb5f32a036363b06e7bfb87b4e34e0be64044
Leboucetmistere <arthurdp96@hotmail.fr>
2015-08-13 15:07:54 +0200

    Added the DrDobbs super useful and genuine Messaging System of Maciej Sobczak, with some modification. Example files update will follow to use it. http://www.drdobbs.com/jvm/java-like-messaging-system-for-c-classes/184403960?pgno=1

---
+++ Sources/ECS/Messaging.cpp
@@ -0,0 +1,9 @@
+//
+//  Messaging.cpp
+//  Simple Game Framework
+//
+//  Created by DEPASSE Arthur on 13/08/2015.
+//  Copyright (c) 2015 DEPASSE Arthur. All rights reserved.
+//
+
+#include "Messaging.h"
---
+++ Sources/ECS/Messaging.h
@@ -0,0 +1,14 @@
+//
+//  Messaging.h
+//  Simple Game Framework
+//
+//  Created by DEPASSE Arthur on 13/08/2015.
+//  Copyright (c) 2015 DEPASSE Arthur. All rights reserved.
+//
+
+#ifndef __Simple_Game_Framework__Messaging__
+#define __Simple_Game_Framework__Messaging__
+
+#include <stdio.h>
+
+#endif /* defined(__Simple_Game_Framework__Messaging__) */
